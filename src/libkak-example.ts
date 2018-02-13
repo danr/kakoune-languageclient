944b5db0b1a39ce8db443f9f99b9c8352563e502
Dan Rosén <dan.rosen@gu.se>
2018-02-14 00:03:01 +0100

    Add completer to example

--- src/libkak-example.ts
+++ src/libkak-example.ts
@@ -34,3 +34,33 @@ kak.def_with_reply('js-eval-sync', '-params 1', ['1', 'client'], m => {
   console.log(res)
   return res
 })
+
+const all = Object.keys(libkak.Details) as (keyof typeof libkak.Details)[]
+
+kak.def_with_reply('complete', '', all, m => {
+    console.error(m)
+    const completions: libkak.Completion[] = [
+      libkak.Completion('a2', 'help 1', 'a2 (function)'),
+      libkak.Completion('a2', 'help 2', 'a2 (function)'),
+      libkak.Completion('a2', 'help 3', 'a2 (function)'),
+      libkak.Completion('a2', 'help 4', 'a2 (function)'),
+      libkak.Completion('b1', 'help 1', 'b1 (function)'),
+      libkak.Completion('b2', 'help 2', 'b1 (function)'),
+      libkak.Completion('b3', 'help 3', 'b1 (function)'),
+      libkak.Completion('b4', 'help 4', 'b1 (function)'),
+      libkak.Completion('c1', 'help 1', 'c1 (function)'),
+      libkak.Completion('c1', 'help 2', 'c2 (function)'),
+      libkak.Completion('c1', 'help 3', 'c3 (function)'),
+      libkak.Completion('c1', 'help 4', 'c4 (function)'),
+    ]
+    const reply = libkak.complete_reply('libkak_completions',
+      {...(m as any), cursor_column: m.cursor_column-1, completions})
+    console.error({reply})
+    return reply
+  })
+kak.msg(`
+    hook global InsertChar [a-z] %{
+        complete
+    }
+`)
+
